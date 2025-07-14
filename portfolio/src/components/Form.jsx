9b5843243cfe127cadcd55f6ac4207cb731ba8d2
Pedro Vicente <pedro.costa.vicente@tecnico.ulisboa.pt>
2025-07-14 16:29:07 +0100

    Portfolio is done

--- portfolio/src/components/Form.jsx
+++ portfolio/src/components/Form.jsx
@@ -1,20 +1,58 @@
 import { useState } from "react";
 import './Form.css';
 
+
+
+function TelephoneError(){
+    return(
+        <>
+            <p>Invalid telephone Number!</p>
+        </>
+    );
+}
+
+function EmailError(){
+    return(
+        <>
+            <p>Invalid email address!</p>
+        </>
+    )
+}
 function Form(){
     const [firstName, setFirstName] = useState(""); 
     const [lastName, setLastName] = useState(""); 
-    const [email, setEmail] = useState("");
+    const [email, setEmail] = useState({address: "", valid: false});
     const [role, setRole] = useState("role");
-    const [telephone, setTelephone] = useState("")  
+    const [telephone, setTelephone] = useState({number:"", valid: false})  
     const [message, setMessage] = useState("");
 
+    const validateEmail = (email) => email.includes("@");
 
-    const isValidForm = () => {
+    const validateTelephone =(tel)=>{
+        let valid = true;
+
+        tel = tel.replace(/\s+/g, "");
+
+        if(tel[0] === "+") tel = tel.slice(1);
+        for(let c of tel){
+            if(c < '0' || c >'9'){
+                valid = false;
+                break;
+            }
+        }
+        console.log(valid);
+        
+        return valid;
+    }
+
+    const isValidForm = (e) => {
         return ( 
             firstName &&
             lastName && 
-            role !== "role" 
+            role !== "role" &&
+            telephone.valid &&
+            email.valid &&
+            message
           ); 
     }
 
@@ -60,21 +98,29 @@ function Form(){
                     <label>Telephone</label>
                     <input 
                         type="telephone" 
-                        value={telephone} 
+                        value={telephone.number} 
                         onChange={(e) =>{
-                            setTelephone(e.target.value);
+                            setTelephone({...telephone, 
+                                        number: e.target.value,
+                                        valid: validateTelephone(e.target.value)});
                         }}
                         placeholder="Telephone number" />
+                    {telephone.number && (!telephone.valid || telephone.number.length < 9 || telephone.number.length >13) &&
+                    (( <small>⚠️Invalid telephone format⚠️</small>)) }
                 </div>
                 <div className="Field">
                     <label>Email</label>
                     <input 
                         type="email" 
-                        value={email} 
+                        value={email.address} 
                         onChange={(e) =>{
-                            setEmail(e.target.value);
+                            setEmail({...email,
+                                    address: e.target.value,
+                                    valid: validateEmail(e.target.value)});
                         }}
                         placeholder="Email Address" />
+                    {email.address && !email.valid && 
+                    ( <small>⚠️Invalid email format⚠️</small>)}
                 </div>
                 <div className="Field"> 
                 <label> 
@@ -98,8 +144,8 @@ function Form(){
                 </div>
             </div>
             <div className="container">
-                <button type="reset" onClick={clearForm}>Clear</button>
-                <button type="submit" disabled={!isValidForm()}>Send Message!</button> 
+                <button type="reset" className="Reset" onClick={clearForm}>Clear</button>
+                <button type="submit" className="Submit" disabled={!isValidForm()}>Send Message!</button> 
             </div>
         </div>
     );
--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,10 +1,32 @@
+import { useState,useEffect } from 'react';
 import './Header.css'
 import WebNav from './WebNav';
+
 function Header(){
+    const [hideHeader, setHideHeader] = useState(false);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY && currentScrollY > 50) {
+            setHideHeader(true); 
+        } else {
+            setHideHeader(false); 
+        }
+
+        setLastScrollY(currentScrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [lastScrollY]);
 
     return(
         <>
-            <header className="Header">
+            <header className={`${hideHeader ? 'Hide' : 'Header'}`}>
                 <h1>The Engineer's Kernel</h1>
                 <WebNav/>
             </header>
